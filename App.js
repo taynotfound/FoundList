5936bc8e4f9e408219a0c4b76c8e1d571ca9b18c
taynotfound <marztayron@gmail.com>
2024-09-29 19:45:29 +0200

    fix: Colors back to being persistent

--- App.js
+++ App.js
@@ -2,16 +2,17 @@ import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons'; // Ensure Ionicons is imported
-import SettingsScreen from './SettingsScreen'; // Import the SettingsScreen
-import ResolvedScreen from './ResolvedScreen'; // Import the ResolvedScreen
-import PendingScreen from './PendingScreen'; // Import the PendingScreen
-import CustomToast from './CustomToast'; // Ensure you import your CustomToast component
-import { getTextColor } from './colorUtils'; // Import the utility function
+import { Ionicons } from '@expo/vector-icons';
+import SettingsScreen from './SettingsScreen';
+import ResolvedScreen from './ResolvedScreen';
+import PendingScreen from './PendingScreen';
+import CustomToast from './CustomToast';
+import { getTextColor } from './colorUtils';
+import { lightTheme, darkTheme } from './themes';
+import LoadingScreen from './LoadingScreen'; // Import the LoadingScreen component
 
 const Tab = createBottomTabNavigator();
 
-// Define default colors
 const defaultColors = {
   background: '#153238',
   inputBackground: '#fff',
@@ -27,33 +28,42 @@ export default function App() {
   const [toastVisible, setToastVisible] = useState(false);
   const [colors, setColors] = useState(defaultColors);
   const [theme, setTheme] = useState('Sunset Bloom');
+  const [isLightMode, setIsLightMode] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // Manage loading state
 
   useEffect(() => {
     const loadData = async () => {
       try {
-        console.log(colors)
-
         const storedTodos = await AsyncStorage.getItem('todos');
-        console.log(storedTodos);
         const storedColors = await AsyncStorage.getItem('colors');
         const storedResolvedTodos = await AsyncStorage.getItem('resolvedTodos');
         const storedTheme = await AsyncStorage.getItem('theme');
+        const storedMode = await AsyncStorage.getItem('isLightMode');
+
         if (storedTodos) {
           const parsedTodos = JSON.parse(storedTodos);
           const formattedTodos = parsedTodos.map(todo => ({
             id: todo.id,
-            text: todo.text.text, // Adjust this if necessary
+            text: todo.text.text,
             priority: todo.text.priority,
             date: todo.text.date,
           }));
           setTodos(formattedTodos);
         }
-        console.log(storedColors);
-        if (storedColors) setColors(colors);
+
+        if (storedTheme) setTheme(storedTheme);
+        if (storedMode !== null) setIsLightMode(storedMode === 'true');
+
+        const selectedColors = (storedMode === 'true')
+          ? lightTheme[storedTheme || 'Sunset Bloom']
+          : darkTheme[storedTheme || 'Sunset Bloom'];
+
+        setColors(storedColors ? JSON.parse(storedColors) : selectedColors);
         if (storedResolvedTodos) setResolvedTodos(JSON.parse(storedResolvedTodos));
-        if(storedTheme) setTheme(storedTheme);
+        setTimeout(() => setIsLoading(false), 300); // Set loading to false after 1 second
       } catch (error) {
         console.error('Failed to load data:', error);
+        setIsLoading(false); // Even if there’s an error, we need to stop loading
       }
     };
 
@@ -61,18 +71,41 @@ export default function App() {
   }, []);
 
   const addTodo = async (text, priority) => {
-    const newTodo = { id: Date.now(), text: text, priority: priority, date: new Date().toISOString() }; // Ensure all properties are correct
+    const newTodo = { id: Date.now(), text: text, priority: priority, date: new Date().toISOString() };
     const updatedTodos = [...todos, newTodo];
     setTodos(updatedTodos);
     await AsyncStorage.setItem('todos', JSON.stringify(updatedTodos));
   };
-    
-  const updateThemeMode = async (theme, mode, lightTheme, darkTheme) => {
-    const updatedColors = mode === 'light' ? lightTheme[theme] : darkTheme[theme];
+
+  const updateThemeMode = async (newTheme, mode) => {
+    const updatedColors = mode === 'light' ? lightTheme[newTheme] : darkTheme[newTheme];
     setColors(updatedColors);
+    setTheme(newTheme);
+    setIsLightMode(mode === 'light');
     await AsyncStorage.setItem('colors', JSON.stringify(updatedColors));
-    await AsyncStorage.setItem('theme', theme);
-  }
+    await AsyncStorage.setItem('theme', newTheme);
+    await AsyncStorage.setItem('isLightMode', mode === 'light' ? 'true' : 'false');
+  };
+
+  const unresolveTodo = async (id) => {
+    const todoToUnresolve = resolvedTodos.find((todo) => todo.id === id);
+    if (todoToUnresolve) {
+      // Remove from resolvedTodos and add back to todos
+      const updatedResolvedTodos = resolvedTodos.filter((todo) => todo.id !== id);
+      const updatedTodos = [...todos, todoToUnresolve];
+      
+      setResolvedTodos(updatedResolvedTodos);
+      setTodos(updatedTodos);
+      
+      // Update AsyncStorage
+      await AsyncStorage.setItem('resolvedTodos', JSON.stringify(updatedResolvedTodos));
+      await AsyncStorage.setItem('todos', JSON.stringify(updatedTodos));
+      
+      showToast('Todo moved back to pending!');
+    }
+  };
+  
+
   const resolveTodo = async (id) => {
     const todoToResolve = todos.find((todo) => todo.id === id);
     if (todoToResolve) {
@@ -99,29 +132,18 @@ export default function App() {
     setTimeout(() => {
       setToastVisible(false);
       setToastMessage('');
-    }, 2000); // Hide toast after 2 seconds
+    }, 2000);
   };
 
-  const textColor = getTextColor(colors.background); // Get text color based on background
+  const textColor = getTextColor(colors.background);
 
-  // Function to render tab icons
-  const renderTabIcon = (routeName) => {
-    let iconName;
-
-    if (routeName === 'Pending') {
-      iconName = 'md-list'; // Replace with the desired icon name
-    } else if (routeName === 'Resolved') {
-      iconName = 'md-checkmark-circle'; // Replace with the desired icon name
-    } else if (routeName === 'Settings') {
-      iconName = 'md-settings'; // Replace with the desired icon name
-    }
-
-    return <Ionicons name={iconName} size={24} color={textColor} />;
-  };
+  if (isLoading) {
+    return <LoadingScreen colors={colors} />; // Show loading screen while initializing
+  }
 
   return (
     <NavigationContainer>
-        <Tab.Navigator
+      <Tab.Navigator
         screenOptions={{
           tabBarStyle: { backgroundColor: colors.background },
           tabBarActiveTintColor: textColor,
@@ -141,7 +163,7 @@ export default function App() {
               showToast={showToast} 
               colors={colors} 
             />
-          )} 
+          )}
           options={{
             tabBarIcon: ({ color }) => (
               <Ionicons name="list" size={24} color={color} />
@@ -156,15 +178,16 @@ export default function App() {
               deleteTodo={deleteTodo}
               showToast={showToast}
               colors={colors}
+              unresolveTodo={unresolveTodo}
             />
-          )} 
+          )}
           options={{
             tabBarIcon: ({ color }) => (
               <Ionicons name="checkmark-done" size={24} color={color} />
             ),
           }}
         />
-         <Tab.Screen 
+        <Tab.Screen 
           name="Settings" 
           children={() => (
             <SettingsScreen
@@ -174,9 +197,9 @@ export default function App() {
               showToast={showToast}
               theme={theme}
               updateThemeMode={updateThemeMode}
+              isLightMode={isLightMode}
             />
           )}
-         
           options={{
             tabBarIcon: ({ color }) => (
               <Ionicons name="settings" size={24} color={color} />
